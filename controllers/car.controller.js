0a05b5b10f8e147f19a0c0e418451f4b31bb5ce7
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-05-21 15:45:52 +0700

    1st

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,46 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+  } catch (err) {
+    // YOUR CODE HERE
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  // 	{
+  //     "message": "Get Car List Successfully!",
+  //     "cars": [
+  //         //car objects
+  //     ],
+  //     "page": 1,
+  //     // total pages
+  //     "total": 1192
+  // }
+  try {
+    // YOUR CODE HERE
+  } catch (err) {
+    // YOUR CODE HERE
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+  } catch (err) {
+    // YOUR CODE HERE
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    // YOUR CODE HERE
+  } catch (err) {
+    // YOUR CODE HERE
+  }
 };
 
 module.exports = carController;
